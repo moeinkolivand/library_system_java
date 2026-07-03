48cd65009d89ea52e638d474ef75d4f5c3045493
moein <moeinkolivand97@gmail.com>
2026-07-03 18:57:14 +0330

    complete functionality library

--- src/main/java/com/tutorial/library/Library.java
+++ src/main/java/com/tutorial/library/Library.java
@@ -6,16 +6,97 @@ public class Library {
     private List<Book> books;
     private List<Member> members;
 
-    public List<Book> listAvailableBooks() {
-        return List.of();
+    public void listAvailableBooks() {
+        for (Book bok : this.books) {
+            if (bok.isAvailable()) {
+                System.out.println(bok);
+            }
+        }
     };
 
-    public void returnBook(String isbn, String memberId) {}
+    public void returnBook(String isbn, String memberId) {
+        Book book;
+        Member member;
+        boolean mmbrFounded = false;
+        boolean bookFounded = false;
+        for (Book bok : this.books) {
+            if (bok.getIsbn().equals(isbn)) {
+                if (bok.isAvailable()) {
+                    bookFounded = bok.getIsbn().equals(isbn);
+                    book = bok;
+                    for (Member mmbr : this.members) {
+                        if (mmbr.getMemberId().equals(memberId)) {
+                            member = mmbr;
+                            mmbrFounded = true;
+                            List<Book> memberBorrowedBooks = member.getborrowedBooks();
+                            for (Book mmbrBook: memberBorrowedBooks) {
+                                if (mmbrBook.getIsbn().equals(isbn)) {
+                                    System.out.println("Member With Id " + memberId + " " + "Already Have Book With Isbn: " + isbn);
+                                    return;
+                                }
+                                member.returnBook(book);
+                                bok.setAvailable(true);
+                            }
+                        }
+                    }
+                    if (!mmbrFounded) {
+                        System.out.println("Member Does Not Exists !");
+                        return;
+                    }
+                }
+            }
+        }
+        if (!bookFounded) {
+            System.out.println("Book Does Not Exists !");
+        }
+    }
 
-    public void borrowBook(String isbn, String memberId) {}
-    public void registerMember(Member member) {}
+    public void borrowBook(String isbn, String memberId) {
+        Book book;
+        Member member;
+        boolean mmbrFounded = false;
+        boolean bookFounded = false;
+        for (Book bok : this.books) {
+            if (bok.getIsbn().equals(isbn)) {
+                if (bok.isAvailable()) {
+                    bookFounded = bok.getIsbn().equals(isbn);
+                    book = bok;
+                    for (Member mmbr : this.members) {
+                        if (mmbr.getMemberId().equals(memberId)) {
+                            member = mmbr;
+                            mmbrFounded = true;
+                            List<Book> memberBorrowedBooks = member.getborrowedBooks();
+                            if (memberBorrowedBooks.size() >= 3) {
+                                System.out.println("Member Already Have 3 Books");
+                            }
+                            for (Book mmbrBook: memberBorrowedBooks) {
+                                if (mmbrBook.getIsbn().equals(isbn)) {
+                                    System.out.println("Member With Id " + memberId + " " + "Already Have Book With Isbn: " + isbn);
+                                    return;
+                                }
+                                member.borrowBook(book);
+                                bok.setAvailable(false);
+                            }
+                        }
+                    }
+                    if (!mmbrFounded) {
+                        System.out.println("Member Does Not Exists !");
+                        return;
+                    }
+                }
+            }
+        }
+        if (!bookFounded) {
+            System.out.println("Book Does Not Exists !");
+        }
+    }
+    public void registerMember(Member member) {
+        this.members.add(member);
+    }
 
-    public void addBook(Book book) {}
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
 
 
 }
