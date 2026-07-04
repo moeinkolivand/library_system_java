e6d1d2e3f2a21a0c357c3fdaed80d3f013233325
moein <moeinkolivand97@gmail.com>
2026-07-04 15:48:05 +0330

    refactor the library class with java 8 functions

--- src/main/java/com/tutorial/library/Library.java
+++ src/main/java/com/tutorial/library/Library.java
@@ -2,6 +2,7 @@ package com.tutorial.library;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Library {
     private List<Book> books;
@@ -26,70 +27,32 @@ public class Library {
     }
 
     public void returnBook(String isbn, String memberId) {
-        Book book;
-        Member member;
-        for (Book bok : this.books) {
-            if (bok.getIsbn().equals(isbn)) {
-                if (!bok.isAvailable()) {
-                    book = bok;
-                    for (Member mmbr : this.members) {
-                        if (mmbr.getMemberId().equals(memberId)) {
-                            member = mmbr;
-                            List<Book> memberBorrowedBooks = member.getBorrowedBooks();
-                            for (Book mmbrBook: memberBorrowedBooks) {
-                                if (mmbrBook.getIsbn().equals(isbn)) {
-                                    member.returnBook(book);
-                                    bok.setAvailable(true);
-                                    System.out.println("The Book With Isbn : " + isbn + " Returned");
-                                    return;
-                                }
-                            }
-                            throw new BookNotBorrowedByThisUserException("Book Not Borrowed By This User !");
-                        }
-                    }
-                    throw new MemberDoesNotExistsException("Member Does Not Exists");
-                } else {
-                    System.out.println("The Book Is Available And Not Borrowed By Any User");
-                    return;
-                }
-            }
+        Book book = findBookByIsbn(isbn);
+        Member member = findMemberById(memberId);
+        if (!member.getBorrowedBooks().contains(book)) {
+            throw new BookNotBorrowedByThisUserException("Book With Isbn " + isbn + "Dosnt Borrowed By This User !");
         }
-        throw new BookDoesNotExistsException("Book Does Not Exists");
+        member.returnBook(book);
+        book.setAvailable(true);
+        System.out.println("Book '" + isbn + "' returned by member '" + memberId + "'");
     }
 
     public void borrowBook(String isbn, String memberId) {
-        Book book;
-        Member member;
-        for (Book bok : this.books) {
-            if (bok.getIsbn().equals(isbn)) {
-                if (bok.isAvailable()) {
-                    book = bok;
-                    for (Member mmbr : this.members) {
-                        if (mmbr.getMemberId().equals(memberId)) {
-                            member = mmbr;
-                            List<Book> memberBorrowedBooks = member.getBorrowedBooks();
-                            if (memberBorrowedBooks.size() >= 3) {
-                                throw new MaximumBorrowedBookException("Member Already Borrowed Three Book");
-                            }
-                            for (Book mmbrBook: memberBorrowedBooks) {
-                                if (mmbrBook.getIsbn().equals(isbn)) {
-                                    System.out.println("Member With Id " + memberId + " " + "Already Have Book With Isbn: " + isbn);
-                                    return;
-                                }
-                            }
-                            member.borrowBook(book);
-                            bok.setAvailable(false);
-                            System.out.println("Book With Isbn " + isbn + "Borrowed For Member With Id " + memberId);
-                            return;
-                        }
-                    }
-                    throw new MemberDoesNotExistsException("Member Does Not Exists");
-                } else {
-                    throw new BookNotAvailableException("Book Is Not Available");
-                }
-            }
+        Book book = findBookByIsbn(isbn);
+        Member member = findMemberById(memberId);
+        if (!book.isAvailable()) {
+            throw new BookNotAvailableException("Book With Isbn" + isbn + "Is Not Available");
         }
-        throw new BookDoesNotExistsException("Book Does Not Exists");
+        if (member.getBorrowedBooks().size() >= 3) {
+            throw new MaximumBorrowedBookException("Member '" + memberId + "' has reached the 3-book limit");
+        }
+        if (member.getBorrowedBooks().contains(book)) {
+            System.out.println("Member already has this book");
+            return;
+        }
+        member.borrowBook(book);
+        book.setAvailable(false);
+        System.out.println("Book '" + isbn + "' borrowed by member '" + memberId + "'");
     }
     public void registerMember(Member member) {
         for (Member mmbr: this.members) {
@@ -103,5 +66,13 @@ public class Library {
 
     public void addBook(Book book) {
         this.books.add(book);
+    }
+
+    private Book findBookByIsbn(String isbn) {
+        return books.stream().filter(book -> book.getIsbn().equals(isbn)).findFirst().orElseThrow(() -> new BookDoesNotExistsException("Book With Isbn " + isbn + "Not Founded"));
+    }
+
+    private Member findMemberById(String id) {
+        return members.stream().filter(member -> member.getMemberId().equals(id)).findFirst().orElseThrow(() -> new MemberDoesNotExistsException("Member With Id " + id + "Does Not Exists"));
     }
 }
